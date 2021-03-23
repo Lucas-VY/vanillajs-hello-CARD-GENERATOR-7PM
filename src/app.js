bbb30521511249f0c4009218d2b4b28818a69e7d
LucasVY <lucasvy@LucasVYs-MacBook-Pro.local>
2021-03-23 14:51:10 -0300

    card color terminado

--- src/app.js
+++ src/app.js
@@ -1,32 +1,24 @@
-/* eslint-disable */
-window.onload = function() {
-  //write your code here
-let randomSymbol = generateRandomSuit();
-document.querySelector('#symbol1').innerHTML = randomSymbol;
-document.querySelector('#symbol2').innerHTML = randomSymbol;
-document.querySelector('.middle-number').innerHTML = generateRandomNumber();
+window.onload = function () {
+  let randomSymbol = generateRandomSuit();
+  if(randomSymbol === "♥" || randomSymbol === "♦"){
+    document.querySelector('#symbol1').style.color = "red";
+    document.querySelector('#symbol2').style.color = "red";
+  }
+  document.querySelector('#symbol1').innerHTML = randomSymbol;
+  document.querySelector('#symbol2').innerHTML = randomSymbol;
+  document.querySelector('.middle-number').innerHTML = generateRandomNumber();
+
 };
 
-let generateRandomNumber = () =>{
-  let numbers = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
-  let indexNumbers = Math.floor(Math.random() * numbers.length); 
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
 };
 
-let generateRandomSuit = () =>{
-  let suit = ["♥","♦","♠","♣"]; 
+let generateRandomSuit = () => {
+  let suit = ["♥", "♦", "♠", "♣"];
   let indexSuit = Math.floor(Math.random() * suit.length);
+  
   return suit[indexSuit];
-
 };
-
- 
-
-
-
-
-
-
-
-
-
